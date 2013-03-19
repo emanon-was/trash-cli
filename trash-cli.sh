fb97422fd6e5b2731ce4d6dfdc9b9e34cda33708
emanon-was <nicosm000@gmail.com>
2013-03-20 05:02:21 +0900

    create trash-put trash-empty trash-list

---
+++ trash-cli.sh
@@ -0,0 +1,139 @@
+abspath ()
+{
+    #
+    # Return the absolute path
+    #
+    IFS=$'\n';
+    for arg in $@;do
+        # file
+        if [ -f $arg ];then
+            f=`basename $arg;`;
+            p=`dirname $arg;`;
+            p=`cd $p;pwd;`;
+            abs=$p/$f;
+            echo $abs;
+        fi
+        # directory
+        if [ -d $arg ];then
+            abs=`cd $arg;pwd`;
+            echo $abs;
+        fi
+    done
+    unset arg f p abs IFS;
+    return 0;
+}
+
+decode_utf8 ()
+{
+    #
+    # Decode to UTF-8 (Use 'echo -e')
+    #
+    IFS=$'\n'
+    for out in $@;do
+        out=`echo -e $out | sed -e "s/%\([ 0-9a-fA-F][ 0-9a-fA-F]\)/\\\\\x\1/g"`;
+        echo -e "$out";
+    done
+    unset out IFS;
+}
+
+trash-put ()
+{
+    #
+    # Move files to Trash and Create a trashinfo
+    #
+    # values
+    IFS=$'\n';
+    trash=~/.local/share/Trash;
+    date=`date +"%Y-%m-%dT%T"`;
+    files=(`abspath $@`);
+    # mkdir ~/.local/share/Trash
+    if [ -e $trash/files ];then mkdir -p $trash/files;fi
+    if [ -e $trash/info ] ;then mkdir -p $trash/info ;fi
+    # processing
+    for fp in ${files[*]};do
+        # rename
+        rname=`basename $fp`;
+        if [ -f $fp ];then
+            declare -i fnum;
+            fname=`echo $rname|sed -e "s/\([^.]*\)\(.*\)/\1/"`;
+            fext=`echo $rname|sed -e "s/\([^.]*\)\(.*\)/\2/"`;
+            fnum=`find $trash/files -regex ".*\/$fname\(\.[0-9]*\)?$fext"|wc -l`+1;
+            if [ $fnum -gt 1 ];then
+                rname=$fname.$fnum$fext;
+            fi
+            unset fname fext fnum;
+        fi
+        if [ -d $fp ];then
+            declare -i fnum;
+            fnum=`find $trash/files -regex ".*\/$rname\(\.[0-9]*\)?"|wc -l`+1;
+            if [ $fnum -gt 1 ];then
+                rname=$rname.$fnum;
+            fi
+            unset fnum;
+        fi
+        # move file to Trash/files
+        mv $fp $trash/files/$rname;
+        # create trashinfo
+        trashinfo=$trash/info/$rname.trashinfo
+        echo "[Trash Info]"        > $trashinfo
+        echo "Path=$fp"           >> $trashinfo
+        echo "DeletionDate=$date" >> $trashinfo
+    done
+    unset IFS trash date files fp rname trashinfo;
+}
+
+trash-list ()
+{
+    #
+    # All arguments are passed to the sort command to be executed at the end.
+    #
+    # values
+    IFS=$'\n';
+    trash=~/.local/share/Trash;
+    if [ -e $trash/files ] && [ -e $trash/info ];then
+        info=(`find $trash/info -name '*.trashinfo'`);
+        if [ ${#info[*]} -ne 0 ];then
+            out="`cat ${info[*]}\
+                  | grep -v '\[Trash Info\]'\
+                  | sed  -e '/Path=/{;N;s/Path=\(.*\)\nDeletionDate=\(.*\)T\(.*\)/\2 \3 \1/g;}'`";
+            decode_utf8 $out | sort $@;
+        fi
+    fi
+    unset trash out info IFS;
+}
+
+trash-empty ()
+{
+    # values
+    IFS=$'\n';
+    trash=~/.local/share/Trash;
+    declare -i ago;ago=$1;
+    insert=`date +"%Y-%m-%d %T" -d "$ago days ago"`
+    date=`date +"%Y%m%d%H%M%H" -d "$ago days ago"`;
+
+    # message
+    echo -e "`trash-list`\n$insert"\
+    | sort\
+    | grep "^$insert$" -B 10000 | grep -v "^$insert$";
+    echo -n "Delete these files really? [y/n] ";
+    read ans;
+
+    # delete
+    if [ $ans = 'y' ] || [ $ans = 'yes' ];then
+        info=(`find $trash/info -name '*.trashinfo'`);
+        rm=(-rf);
+        for i in ${info[*]};do
+            file=`basename $i|sed -e "s/\.trashinfo//"`;
+            file=$trash/files/$file;
+            if [ -e $file ];then
+                t=`sed -n "/DeletionDate=.*/p" $i | sed -e "s/DeletionDate=//;s/-//g;s/T//g;s/\://g;"`;
+                if [ $date -gt $t ];then
+                    rm=(${rm[*]} $i $file);
+                fi
+            fi
+        done
+        rm ${rm[*]};
+    fi
+    unset IFS trash ago insert date ans info i file t rm;
+}
+
